2a04db5c28d9e9d36c9ec9a782112e86395bf804
Prabukhanth T <prabu@criodo.com>
2024-09-14 15:30:31 +0530

    countries

---
+++ countries/src/App.js
@@ -0,0 +1,9 @@
+import Countries from './Countries'; 
+
+function App() {
+  return <div className="App">
+    <Countries />
+    </div>;
+}
+
+export default App;
---
+++ countries/src/Countries.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+
+const Tile = ({flagUrl, name, altFlag}) => {
+    return (
+        <div 
+        style={{
+            display:"flex",
+            justifyContent:"center",
+            alignItems: "center",
+            margin: "10px",
+            padding: "10px",
+            border: "1px solid black",
+            borderRadius: "8px",
+            msFlexDirection: "column",
+            width: "200px",
+        }}
+        >
+        <img 
+        src={flagUrl}
+        alt={altFlag}
+        style ={{width: "100px", height:"100px"}}
+        />
+        <h2>name</h2>
+        </div>
+    )
+}
+    
+function Countries () {
+    const API_URL = "https://restcountries.com/v3.1/all";
+    const [Countries, setCountries] = useState ([]);
+
+    useEffect (() => {
+        fetch (API_URL)
+        .then ((response) => response.json())
+        .then ((data) => setCountries(data));
+    }, []);
+
+return (
+        <div
+        style = {{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            flexWrap: "wrap",
+
+        }}
+        >
+            {Countries.map((country) => (
+                <Tile
+                key ={country.cca3}
+                flagUrl={country.flags.png}
+                name={country.name.common}
+                altFlag={country.flags.alt} 
+                /> 
+            ))}
+
+            
+        </div>
+    );
+}
+export default Countries;
